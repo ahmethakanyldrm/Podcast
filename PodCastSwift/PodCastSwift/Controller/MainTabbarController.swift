217230f2e91eafc9e428c6e147ee14bf91646bd6
ahmethakanyldrm <65174957+ahmethakanyldrm@users.noreply.github.com>
2023-05-05 15:03:41 +0300

    Proje oluşturuldu.

---
+++ PodCastSwift/PodCastSwift/Controller/MainTabbarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabbarController.swift
+//  PodCastSwift
+//
+//  Created by AHMET HAKAN YILDIRIM on 5.05.2023.
+//
+
+import UIKit
+
+class MainTabbarController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
