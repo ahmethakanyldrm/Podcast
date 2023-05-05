6b918288a38475785e1e64cfa702bc2893385b52
ahmethakanyldrm <65174957+ahmethakanyldrm@users.noreply.github.com>
2023-05-05 15:34:24 +0300

    SearchbarController ve TableView Eklendi.

--- PodCastSwift/PodCastSwift/Controller/SearchViewController.swift
+++ PodCastSwift/PodCastSwift/Controller/SearchViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UITableViewController {
     // MARK: - Properties
+    
+    private let reuseIdentifier = "Search"
     // MARK: - LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +23,35 @@ class SearchViewController: UIViewController {
 
 extension SearchViewController{
     private func style() {
-        view.backgroundColor = .green
-
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        let searchController = UISearchController(searchResultsController: nil)
+        self.navigationItem.searchController = searchController
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
     }
     
     private func layout(){
+        
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SearchViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
     }
 }
